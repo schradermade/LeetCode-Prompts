670e9b3f42b5a06fc1ac2dcc8030fc65697ff0cc
Nathan Schrader <nathanschrader@icloud.com>
2021-03-15 18:53:44 -0700

    add passing solution LC125_isPalindrome w/tests all passing

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,19 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  // Sanitize input string w/REGEX
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+
+  let left = 0;
+  let right = s.length -1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
 
 module.exports = isPalindrome;
