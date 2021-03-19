59bb10dfdb8d732b5e09340f0d4fc3425455e1a4
Nathan Schrader <nathanschrader@icloud.com>
2021-03-18 18:04:30 -0700

    add passing solution LC49_groupAnagrams with test

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,20 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+  let grouped = {};
+
+  for (const word of strs) {
+    const key = word
+      .split('')
+      .sort()
+      .join('');
+
+    if (key in grouped) {
+      grouped[key].push(word)
+    } else {
+      grouped[key] = [word]
+    }
+  }
+
+  return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
