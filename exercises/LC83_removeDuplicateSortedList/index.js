ef94078d189d9b42af2c923a50e8c5152947f4a5
Nathan Schrader <nathanschrader@icloud.com>
2021-03-24 14:56:35 -0700

    add passing solution for LC83

---
+++ exercises/LC83_removeDuplicateSortedList/index.js
@@ -0,0 +1,12 @@
+var deleteDuplicates = function(head) {
+  let current = head;
+  
+  while (current != null && current.next != null) {
+      if (current.next.val == current.val) {
+          current.next = current.next.next;
+      } else {
+          current = current.next;
+      }
+  }
+  return head;
+};
